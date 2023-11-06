e058c805cd6f21e5cb6fb5142783f5f3ee91498a
Sara <saramihajlovic94@gmail.com>
2023-11-06 15:03:47 +0100

    projects initialized

---
+++ driveBack/src/main/java/com/example/driveBack/DriveBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.driveBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveBackApplication.class, args);
+	}
+
+}
